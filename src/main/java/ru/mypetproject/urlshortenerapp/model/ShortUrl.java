0d841067e86b2ef94ec36ec4783078e748a1011c
AvargX <avargx@gmail.com>
2025-06-23 14:52:28 +0900

    feat: add ShortUrl entity

---
+++ src/main/java/ru/mypetproject/urlshortenerapp/model/ShortUrl.java
@@ -0,0 +1,27 @@
+package ru.mypetproject.urlshortenerapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ShortUrl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int Id;
+    @Column(nullable = false, unique = true)
+    public String originalUrl;
+    @Column(nullable = false)
+    public String shortKey;
+    @Column(nullable = false)
+    LocalDateTime createdAt;
+
+}
