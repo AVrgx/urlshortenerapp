8d4ccd5732a82a264c2852d44663f1c02d8c41ac
AvargX <avargx@gmail.com>
2025-06-23 15:02:01 +0900

    feat: add ShortUrlRepository

---
+++ src/main/java/ru/mypetproject/urlshortenerapp/repository/ShortUrlRepository.java
@@ -0,0 +1,14 @@
+package ru.mypetproject.urlshortenerapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.mypetproject.urlshortenerapp.model.ShortUrl;
+
+import java.util.Optional;
+
+@Repository
+public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
+    Optional<ShortUrl> findByShortKey(String shortKey);
+    Optional<ShortUrl> findByOriginalUrl(String originalUrl);
+    boolean existsByShortKey(String shortKey);
+}
