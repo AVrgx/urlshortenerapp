cbcb33af3ef7e1899a7d7ae4c03bb322f111e30c
AvargX <avargx@gmail.com>
2025-06-23 16:14:14 +0900

    feat: add UrlShortenerController and ShortenRequest dto

---
+++ src/main/java/ru/mypetproject/urlshortenerapp/controller/UrlShortenerController.java
@@ -0,0 +1,37 @@
+package ru.mypetproject.urlshortenerapp.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.mypetproject.urlshortenerapp.dto.ShortenRequest;
+import ru.mypetproject.urlshortenerapp.model.ShortUrl;
+import ru.mypetproject.urlshortenerapp.service.UrlShortenerService;
+
+import java.io.InputStream;
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UrlShortenerController {
+    private final UrlShortenerService service;
+
+    @PostMapping("/shorten")
+    public ResponseEntity<String> shorten(@Valid @RequestBody ShortenRequest request) {
+        String shortKey = service.ShortenUrl(request.getUrl());
+        return ResponseEntity.ok(shortKey);
+    }
+
+    @GetMapping("{code}")
+    public ResponseEntity<Void> redirect(@PathVariable String code) {
+        return service.getOriginalUrl(code)
+                .map(originalUrl-> ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(originalUrl))
+                        .<Void>build())
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+}
---
+++ src/main/java/ru/mypetproject/urlshortenerapp/dto/ShortenRequest.java
@@ -0,0 +1,16 @@
+package ru.mypetproject.urlshortenerapp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ShortenRequest {
+    @NotBlank(message = "Запрос не должен быть пустым")
+    @Size(max = 2048, message = "Длинна не должна превышать 2048 символов")
+    @Pattern(regexp = "^(https?://).*", message = "URL должен начинаться с http:// или https://")
+    private String url;
+}
