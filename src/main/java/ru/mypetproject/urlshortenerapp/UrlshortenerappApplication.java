cd41f32e8874f653bbc13c610e52df750ece4e53
AvargX <avargx@gmail.com>
2025-06-30 13:05:51 +0900

    feat: add @EnableScheduling

--- src/main/java/ru/mypetproject/urlshortenerapp/UrlshortenerappApplication.java
+++ src/main/java/ru/mypetproject/urlshortenerapp/UrlshortenerappApplication.java
@@ -2,8 +2,10 @@ package ru.mypetproject.urlshortenerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UrlshortenerappApplication {
 
     public static void main(String[] args) {
